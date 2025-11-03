63864c3c65a6b8a1b88abe986c523287d328f659
Kaidong Hu <hukaidonghkd@gmail.com>
2025-11-02 19:37:38 -0500

    feat(k3s): add firewall and networking configuration
    - Trust CNI and Flannel network interfaces
    - Open required ports for Kubernetes API (6443) and kubelet (10250)
    - Configure Flannel VXLAN (8472) and WireGuard (51820/51821) UDP ports
    - Add iptables rules for k3s pod network (10.42.0.0/16)
    - Add iptables rules for k3s service network (10.43.0.0/16)
    
    Follows k3s official networking requirements:
    https://docs.k3s.io/installation/requirements#networking

--- modules/services/virtualisation.nix
+++ modules/services/virtualisation.nix
@@ -18,4 +18,30 @@
       "--write-kubeconfig-mode=644"
     ];
   };
+
+  # k3s networking and firewall configuration
+  # See: https://docs.k3s.io/installation/requirements#networking
+  networking.firewall = {
+    # Trust CNI and Flannel interfaces
+    trustedInterfaces = [ "cni0" "flannel.1" ];
+
+    allowedTCPPorts = [
+      6443   # Kubernetes API server
+      10250  # Kubelet metrics
+    ];
+
+    allowedUDPPorts = [
+      8472   # Flannel VXLAN
+      51820  # Flannel WireGuard (IPv4)
+      51821  # Flannel WireGuard (IPv6)
+    ];
+
+    extraCommands = ''
+      # Allow all traffic from k3s pod network (10.42.0.0/16)
+      iptables -A nixos-fw -s 10.42.0.0/16 -j nixos-fw-accept -m comment --comment "k3s pod network"
+
+      # Allow all traffic from k3s service network (10.43.0.0/16)
+      iptables -A nixos-fw -s 10.43.0.0/16 -j nixos-fw-accept -m comment --comment "k3s service network"
+    '';
+  };
 }
