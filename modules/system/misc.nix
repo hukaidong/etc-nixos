09ba5868e3f2f8f3953ea4a76b77dcc5b354b90a
Kaidong Hu <hukaidonghkd@gmail.com>
2026-01-17 18:58:25 -0500

    add trusted user for nix setting

--- modules/system/misc.nix
+++ modules/system/misc.nix
@@ -6,10 +6,9 @@
   ];
 
   nix.extraOptions = ''
-    extra-substituters = https://devenv.cachix.org
-    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
     !include ${config.sops.secrets."nix-access-tokens".path}
   '';
 
+  nix.settings.trusted-users = [ "kaidong" ];
   nix.settings.secret-key-files = [ "/etc/nix/digix-nix.key" ];
 }
