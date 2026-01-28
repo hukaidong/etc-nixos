39069d44f3c31c813e76d36697ddb2a0b8523328
Kaidong Hu <hukaidonghkd@gmail.com>
2026-01-28 12:16:46 -0500

    add i3 switch, slim direnv output

--- hosts/kaidong-main-desktop/configuration.nix
+++ hosts/kaidong-main-desktop/configuration.nix
@@ -3,6 +3,9 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
+let
+  usei3 = false;
+in
 {
   nixpkgs.config.allowUnfree = true;
 
@@ -24,7 +27,8 @@
   programs.steam.enable = true;
   programs.steam.protontricks.enable = true;
 
-  kaidong-desktop.desktopEnvironment.plasma6.enable = true;
+  kaidong-desktop.desktopEnvironment.plasma-i3.enable = usei3;
+  kaidong-desktop.desktopEnvironment.plasma6.enable = !usei3;
 
   system.stateVersion = "25.11";
 }
--- modules/programs/environment.nix
+++ modules/programs/environment.nix
@@ -11,4 +11,11 @@
   programs.direnv.enable = true;
   programs.neovim.vimAlias = true;
   programs.nix-ld.enable = true;
+
+  programs.direnv.settings = {
+    global = {
+      warn_timeout = "30s";
+      hide_env_diff = true;
+    };
+  };
 }
