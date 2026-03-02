5f8a80e2f638006893108646ef55baf8413d1550
Kaidong Hu <hukaidonghkd@gmail.com>
2026-03-01 22:29:13 -0500

    remove cursor

--- modules/packages/desktop.nix
+++ modules/packages/desktop.nix
@@ -8,14 +8,12 @@
     mpv
 
     # Fast developing application using unstable packages
-    unstable.code-cursor-fhs
     unstable.antigravity-fhs
 
     # Daily updated nix ai tool sets
     ai-tools.codex
     ai-tools.opencode
     ai-tools.claude-code
-    ai-tools.cursor-agent
 
     # required by opencode
     bun
